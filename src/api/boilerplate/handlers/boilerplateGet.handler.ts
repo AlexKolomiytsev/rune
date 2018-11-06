ef3fe8dad091d23c02592578d24cd0147b4b9d45
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-11-07 01:44:55 +0200

    yarn install development

--- src/api/boilerplate/handlers/boilerplateGet.handler.ts
+++ src/api/boilerplate/handlers/boilerplateGet.handler.ts
@@ -4,6 +4,9 @@ class BoilerplateGetHandler {
   public route(req: Request, res: Response) {
     const { user } = req;
 
+    // eslint-disable-next-line
+    console.log(user);
+
     return res.json({
       boilerplate: 'boilerplate UPDATED 4',
       user,
