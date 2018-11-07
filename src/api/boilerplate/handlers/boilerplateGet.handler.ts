e5b78a76def1aa92650e0695b4e6680a52741db1
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-11-07 02:15:48 +0200

    deployyying

--- src/api/boilerplate/handlers/boilerplateGet.handler.ts
+++ src/api/boilerplate/handlers/boilerplateGet.handler.ts
@@ -4,9 +4,6 @@ class BoilerplateGetHandler {
   public route(req: Request, res: Response) {
     const { user } = req;
 
-    // tslint:disable-next-line
-    console.log(user);
-
     return res.json({
       boilerplate: 'boilerplate UPDATED 4',
       user,
