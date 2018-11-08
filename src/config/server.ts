89094acb08b441f48ade70e5b3adfc6b06529cb0
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-11-08 02:00:57 +0200

    host

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 export default {
-  host: process.env.APP_HOST,
+  host: process.env.APP_HOST || 'localhost',
   port: process.env.PORT || 3030,
 };
