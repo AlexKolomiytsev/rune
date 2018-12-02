1f959ae7be765ad71adcb9c03f5a84eaa84c2d6e
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-12-02 21:56:25 +0200

    handle success queue.process

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -3,7 +3,11 @@ import queues from './queues';
 import processorInitialisers from './processorInitialisers';
 import { logger } from '@app/services';
 
-Object.entries(queues).forEach(([queueName, queue]: [string, Queue.Queue]) => {
-  logger.info(`Worker listening to '${queueName}' queue`);
-  queue.process(processorInitialisers[queueName]);
+Object.entries(queues).forEach(async ([queueName, queue]: [string, Queue.Queue]) => {
+  try {
+    await queue.process(processorInitialisers[queueName]);
+    logger.success(`Worker listening to '${queueName}' queue`);
+  } catch (e) {
+    logger.error(`Worker can't start listen to '${queueName}' queue`, e);
+  }
 });
